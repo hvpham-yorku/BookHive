827a8b37c00ebf10c7f34d9b51b7f8fb48a1ad98
norankerret <norankrt@my.yorku.ca>
2024-11-17 16:37:24 -0500

    backend for borrow books and forgot password

--- website/models.py
+++ website/models.py
@@ -1,8 +1,6 @@
-
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +8,14 @@ class Note(db.Model):
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user2.id'))
 
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    author = db.Column(db.String(150), nullable=False)
+    copies = db.Column(db.Integer, nullable=False)
+    remaining_copies = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user2.id'))
+    borrowed_books = db.relationship('BorrowedBook', backref='book')  
 
 class User2(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,4 +23,14 @@ class User2(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+    books = db.relationship('Book')  
     is_admin = db.Column(db.Boolean, default=False)
+    borrowed_books = db.relationship('BorrowedBook', backref='user')
+
+class BorrowedBook(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user2.id'))  
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))  
+    borrow_date = db.Column(db.DateTime, default=func.now())   
+    due_date = db.Column(db.DateTime)                         
+    returned = db.Column(db.Boolean, default=False)  
