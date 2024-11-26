7ab7019a135c75e105cd12a70b27ee8d0e52498e
Parmoun Khalkhali Sharifi <156559538+parmounks@users.noreply.github.com>
2024-11-26 11:08:11 -0500

    #15 As an admin, I want to generate reports on borrowing statistics so that I can analyze library usage.

--- website/__init__.py
+++ website/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from flask_mail import Mail  # Import Flask-Mail
 from os import path, getenv
 
-
+ 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 mail = Mail()  # Initialize Flask-Mail
