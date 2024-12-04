d81c08d4312420375a4766cf2b33fec26dc4925b
Parmoun Khalkhali Sharifi <156559538+parmounks@users.noreply.github.com>
2024-12-03 21:59:37 -0500

    #1 As a user, I want to register for an account so that I can borrow books.

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,3 +1,21 @@
+/**
+ * deleteBook - Deletes a book record from the server and updates the UI.
+ *
+ * This function triggers a POST request to the server to delete a book identified by its `bookId`.
+ * It prevents the default behavior of the associated event, applies a visual indicator to the delete button, 
+ * and reloads the page upon successful deletion. If the server request fails, an error alert is displayed.
+ *
+ * Workflow:
+ * 1. Prevents the default form or button behavior.
+ * 2. Checks if the button already has the "delete" class to avoid duplicate operations.
+ * 3. Adds a "delete" class to indicate the delete process visually.
+ * 4. Uses a timeout of 1.5 seconds before initiating a fetch request to the `/delete-book/${bookId}` endpoint.
+ * 5. Handles server response:
+ *    - If successful, reloads the page to reflect changes.
+ *    - If unsuccessful, shows an error message.
+ * 6. Logs any errors encountered during the fetch operation.
+ */
+
 function deleteBook(event, bookId) {
     event.preventDefault();
     const button = event.target.closest('.button');
